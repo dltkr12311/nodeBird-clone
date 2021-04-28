c01fc83a39eb46095cfcc1b62642bf126850c862
아이슥 <leesak@aiseug-ui-MacBookPro.local>
2021-04-28 17:30:56 +0900

    add userProfile page 프로필 UI 작업, 회원가입 UI작업 커스텀 훅 추가

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useCallback } from "react";
+import { Card, Avatar, Button } from "antd";
 
-const UserProfile = () => {
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogOut = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
     <>
-      <div>유저프로필</div>
+      <Card
+        actions={[
+          <div key="twit">
+            짹짹 <br />0
+          </div>,
+          <div key="follwings">
+            팔로잉 <br />0
+          </div>,
+          <div key="follwers">
+            팔로워 <br />0
+          </div>,
+        ]}
+      >
+        <Card.Meta avatar={<Avatar>LS</Avatar>} title="LeeSak" />
+        <Button onClick={onLogOut}>로그아웃</Button>
+      </Card>
     </>
   );
 };
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Head from "next/head";
 
+import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowLIst";
+
 const Profile = () => {
+  const followerList = [{ nickname: "이삭" }, { nickname: "토스트" }, { nickname: "레전드" }];
+  const followingList = [{ nickname: "이삭" }, { nickname: "토스트" }, { nickname: "레전드" }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <div>프로필 페이지</div>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
