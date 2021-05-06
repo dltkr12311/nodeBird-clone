64a7c24b82878291bf55cdc7fadbf082aa1213aa
dltkr12311 <leesak94@gmail.com>
2021-05-06 17:26:34 +0900

    add redux-saga 리덕스 사가 설치 및 적용

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -2,20 +2,26 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, createStore, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+
 import reducer from "../reducers";
+import rootSaga from "../sagas";
+
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+  console.log(action);
+  return next(action);
+};
 
 //스토어를 생성하는 함수를 정의
 const configureStore = () => {
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
-  store.dispatch({
-    type: "CHANGE_NICKNAME",
-    data: "boogieLee",
-  });
+  const sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
