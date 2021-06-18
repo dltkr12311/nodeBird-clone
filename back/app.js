eee2e329b4f684707eb4c0253fa204d06b32a8cb
dltkr12311 <leesak94@gmail.com>
2021-06-18 16:48:44 +0900

    add express express설정 및 간단한 api 작성

--- back/app.js
+++ back/app.js
@@ -1,8 +1,31 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.end("Hello node Word");
+const express = require("express");
+const app = express();
+
+//순수 노드는 .end, express는 send
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
-server.listen(3065, () => {
+
+app.get("/api", (req, res) => {
+  res.send("hello api");
+});
+
+app.get("/api/post", (req, res) => {
+  res.json([
+    { id: 1, content: "hello" },
+    { id: 2, content: "hello2" },
+    { id: 3, content: "hello3" },
+  ]);
+});
+
+app.post("/api/post", (req, res) => {
+  res.json({ id: 1, content: "hello" });
+});
+
+app.delete("/api/post", (req, res) => {
+  res.json({ id: 1 });
+});
+
+app.listen(3065, () => {
   console.log("서버 실행중");
 });
